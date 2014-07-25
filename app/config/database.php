5cb9f6f336ea1a9100fe301e340ec47613ee18e2
Nartub <maimar@gmail.com>
2014-07-25 17:15:16 -0300

    db

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'stars',
-			'username'  => '',
-			'password'  => '',
+			'database'  => 'forge',
+			'username'  => 'forge',
+			'password'  => 'LJVbmjiBcZuRA6xpBPEn',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
