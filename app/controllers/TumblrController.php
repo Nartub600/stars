bef5b590e53356587ecd7a486948433521133eee
Nartub <maimar@gmail.com>
2014-07-27 10:29:29 -0300

    mas redes sociales

--- app/controllers/TumblrController.php
+++ app/controllers/TumblrController.php
@@ -2,9 +2,13 @@
 
 class TumblrController extends BaseController {
 
-	public function loginHandler() {
-		$tumblr = $tw = OAuth::consumer('Tumblr');
-		var_dump($tumblr);
-	}
+    public function loginHandler() {
+        $tumblr = $tw = OAuth::consumer('Tumblr');
+        $token = $tumblr->requestRequestToken();
+
+        $url = $tumblr->getAuthorizationUri(array('oauth_token' => $token->getRequestToken()));
+
+        return Redirect::to((string) $url);
+    }
 
 }
